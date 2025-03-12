5a823371ec3acbc3eebad7f259fd0bdf3607069d
Fayaz-4 <shaikfayazbasha936@gmail.com>
2025-03-12 23:49:21 +0530

    shopping-checking-out
    this app is used for checking the shoping details

---
+++ app.js
@@ -0,0 +1,49 @@
+(function() {
+    'use strict';
+
+    angular.module('ShoppingListCheckOff', [])
+    .controller('ToBuyController', ToBuyController)
+    .controller('AlreadyBoughtController', AlreadyBoughtController)
+    .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+    ToBuyController.$inject = ['ShoppingListCheckOffService'];
+    function ToBuyController(ShoppingListCheckOffService) {
+        var toBuy = this;
+        toBuy.items = ShoppingListCheckOffService.getToBuyItems();
+
+        toBuy.buyItem = function(itemIndex) {
+            ShoppingListCheckOffService.buyItem(itemIndex);
+        };
+    }
+
+    AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+    function AlreadyBoughtController(ShoppingListCheckOffService) {
+        var bought = this;
+        bought.items = ShoppingListCheckOffService.getBoughtItems();
+    }
+
+    function ShoppingListCheckOffService() {
+        var service = this;
+        var toBuyItems = [
+            { name: "Cookies", quantity: 10 },
+            { name: "Apples", quantity: 5 },
+            { name: "Milk", quantity: 2 },
+            { name: "Bread", quantity: 1 },
+            { name: "Eggs", quantity: 12 }
+        ];
+        var boughtItems = [];
+
+        service.getToBuyItems = function() {
+            return toBuyItems;
+        };
+
+        service.getBoughtItems = function() {
+            return boughtItems;
+        };
+
+        service.buyItem = function(itemIndex) {
+            var item = toBuyItems.splice(itemIndex, 1)[0];
+            boughtItems.push(item);
+        };
+    }
+})();
